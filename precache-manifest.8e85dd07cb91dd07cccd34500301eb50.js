160c610336bbea548a17f7160721303c5cde04c8
Geidy <geidydm@gmail.com>
2019-07-10 17:41:13 -0400

    Updates

---
+++ precache-manifest.8e85dd07cb91dd07cccd34500301eb50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fa765551d6f0a70aa1648ae564caa5",
+    "url": "/React-Redux-Practice/index.html"
+  },
+  {
+    "revision": "54e12566167f66c6bdba",
+    "url": "/React-Redux-Practice/static/css/main.9c1f8079.chunk.css"
+  },
+  {
+    "revision": "486311938667fa370a60",
+    "url": "/React-Redux-Practice/static/js/2.c04618c8.chunk.js"
+  },
+  {
+    "revision": "54e12566167f66c6bdba",
+    "url": "/React-Redux-Practice/static/js/main.cdf16de7.chunk.js"
+  },
+  {
+    "revision": "1d7219e5c6fd67959b55",
+    "url": "/React-Redux-Practice/static/js/runtime~main.ffd4a7a0.js"
+  }
+]);
